7789c1ca251afb2669d801d5bcdb8e7599817636
KevinModernel <modernelkevin96@gmail.com>
2023-03-06 08:37:54 -0300

    Problem Domain components

---
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,24 @@
+package sudoku.problemdomain;
+
+import sudoku.computationlogic.SudokuUtilities;
+import sudoku.constants.GameState;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+    public static final int GRID_BOUNDARY = 9;
+    public SudokuGame(GameState gameState, int [][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() { // To protect the SudokuGame obj
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+}
